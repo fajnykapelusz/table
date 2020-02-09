561d9efc7d9c14fc3ba026e383cb72af4973392e
Michal <fajnykapelusz@gmail.com>
2020-02-09 19:07:43 +0100

    ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "react-table/react-table.css";
+import ReactTable from "react-table";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: []
+    };
+  }
+  componentDidMount() {
+    const url = "http://rt.ex7.pl/get-data";
+    fetch(url, {
+      method: "POST"
+    })
+      .then(res => res.json())
+      .then(items => {
+        console.log(items);
+        this.setState({ items: items });
+      });
+  }
+  render() {
+    const columns = [
+      {
+        Header: "ID Produktu",
+        accessor: "id",
+        sortable: true
+      },
+      {
+        Header: "Skrót",
+        accessor: "acronym",
+        sortable: true,
+        filterMethod: (filter, row) =>
+          row[filter.id].toLowerCase().startsWith(filter.value),
+        filterable: true
+      },
+      {
+        Header: "Nazwa Produktu",
+        accessor: "name",
+        filterable: true,
+        filterMethod: (filter, row) =>
+          row[filter.id].toLowerCase().startsWith(filter.value),
+        width: 800
+      }
+    ];
+    return (
+      <ReactTable
+        columns={columns}
+        data={this.state.items}
+        defaultPageSize={5}
+      ></ReactTable>
+    );
+  }
 }
 
 export default App;
